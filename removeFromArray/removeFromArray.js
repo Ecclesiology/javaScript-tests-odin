b247315bd6a2760e32e2fffa0e32fc1f7eb77176
Ecclesiology <JMMatthews@Protonmail.com>
2020-03-08 23:10:08 -0400

    Add incomplete removeFromArray test.

---
+++ removeFromArray/removeFromArray.js
@@ -0,0 +1,15 @@
+const removeFromArray = function(input , reject) {
+    let value = new Array
+    let display = ""
+    
+    for ( let i = 0; i <= input.length - 1; i++ ) {
+        let deny = [...reject];
+        value.push(input[i]);
+        display = value.filter(function(accept){
+            return accept != deny
+        });
+    }
+    return display
+}
+
+module.exports = removeFromArray
