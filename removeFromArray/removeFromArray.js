fe0da9ee0e66e0e7f4a5f34ffebad8713b609f06
Ecclesiology <JMMatthews@Protonmail.com>
2020-03-12 16:55:33 -0400

    Test arguments for solution

--- removeFromArray/removeFromArray.js
+++ removeFromArray/removeFromArray.js
@@ -1,9 +1,9 @@
-const removeFromArray = function(input , ...reject) {
+const removeFromArray = function(input , reject) {
     let value = new Array
     let display = ""
     
     for ( let i = 0; i <= input.length - 1; i++ ) {
-        let deny = [...reject];
+        let deny = [...arguments];
         value.push(input[i]);
         display = value.filter(function(accept){
             return accept != deny
