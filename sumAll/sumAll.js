3571d56364f2746f452a0dcb057243e18c43e903
Ecclesiology <JMMatthews@Protonmail.com>
2020-03-19 17:06:51 -0400

    Make function reject negative numbers

--- sumAll/sumAll.js
+++ sumAll/sumAll.js
@@ -3,6 +3,11 @@ const sumAll = function(start, end) {
     if (start > end) {
         start = end + (end = start, 0)
     }
+    
+    if ( start < 0 ) {
+        return "ERROR";
+    } 
+    
     for( i = start; i <= end; i++) {
         sum += +i 
     };
