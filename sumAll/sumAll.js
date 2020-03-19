bd4ae9d24c1a50b200f9af51754b676c5b841e11
Ecclesiology <JMMatthews@Protonmail.com>
2020-03-19 16:48:49 -0400

    Swap starts larger than ends

--- sumAll/sumAll.js
+++ sumAll/sumAll.js
@@ -1,9 +1,13 @@
 const sumAll = function(start, end) {
     let sum = 0;
+    if (start > end) {
+        start = end + (end = start, 0)
+    }
     for( i = start; i <= end; i++) {
         sum += +i 
     };
     return sum
 }
+
 
 module.exports = sumAll
