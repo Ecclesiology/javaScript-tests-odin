65e93b696249afd72e56c3a55cd63de14cbc1f39
Ecclesiology <JMMatthews@Protonmail.com>
2020-03-20 21:58:11 -0400

    Find leap year

--- leapYears/leapYears.js
+++ leapYears/leapYears.js
@@ -1,5 +1,9 @@
-const leapYears = function() {
-
+const leapYears = function(year) {
+    let testLeapYear = year % 4;
+    let checkLeapYear = year % 400;
+    if (testLeapYear == 0) {
+        return true;
+    }
 }
 
 module.exports = leapYears
