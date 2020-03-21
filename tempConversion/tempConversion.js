7dab230763eeed2d0fa9eb8d09278e3211049ec2
Ecclesiology <JMMatthews@Protonmail.com>
2020-03-20 22:49:20 -0400

    Add Fahrenheit to Celsius conversion formula

--- tempConversion/tempConversion.js
+++ tempConversion/tempConversion.js
@@ -1,8 +1,8 @@
-const ftoc = function() {
-
+const ftoc = function(degrees) {
+    return (( degrees - 32 ) * ( 5 / 9 ));
 }
 
-const ctof = function() {
+const ctof = function(temperature) {
 
 }
 
