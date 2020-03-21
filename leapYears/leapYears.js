4ce30a717e960741fb4d345c970592752a141070
Ecclesiology <JMMatthews@Protonmail.com>
2020-03-20 22:06:48 -0400

    Exclude years divisable by 100

--- leapYears/leapYears.js
+++ leapYears/leapYears.js
@@ -1,7 +1,8 @@
 const leapYears = function(year) {
     let testLeapYear = year % 4;
     let checkLeapYear = year % 400;
-    if (testLeapYear == 0) {
+    let isLeapYear = year % 100;
+    if ((testLeapYear == 0) && (isLeapYear !== 0)) {
         return true;
     } else {
         return false;
